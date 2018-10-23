14bc50e43e67cb9278e16fa005ef75a92c1481b8
tomwang1013 <174604588@qq.com>
2018-10-23 14:49:58 +0800

    添加shadow dom草稿

--- source/labs/web-components/main.mjs
+++ source/labs/web-components/main.mjs
@@ -1,10 +1,19 @@
-import { HTMLClockElement } from './clock.mjs'
+// import { HTMLClockElement } from './clock.mjs'
 
-customElements.define('x-clock', HTMLClockElement);
+// customElements.define('x-clock', HTMLClockElement);
 
-document.getElementById('reg').addEventListener('click', (e) => {
-})
+// document.getElementById('reg').addEventListener('click', (e) => {
+// })
 
-customElements.whenDefined('x-clock').then(() => {
-  console.log('x-clock defined');
-});
+// customElements.whenDefined('x-clock').then(() => {
+//   console.log('x-clock defined');
+// });
+
+const div = document.getElementById('to-attach-shadow');
+
+// attach a shadow dom tree to it
+const shadowRoot = div.attachShadow({ mode: 'open' });
+shadowRoot.innerHTML = '<p>shadow p1</p><p>shadow p2</p>';
+
+div.shadowRoot === shadowRoot
+shadowRoot.host === div
